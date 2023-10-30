de08723ab0c6a6bdcd7586b832bffb3e76fee0e1
Ewan Stott <ewan.stott1@gmail.com>
2023-10-30 21:40:05 +0000

    commit

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-// Javascript for About Me section -> Tab changes
-<script>
-  var tablinks = document.getElementsByClassName("tab-links");
-  var tabcontents = document.getElementsByClassName("tab-contents");
-
-  function opentab(tabname) {
-    for (tablink of tablinks) {
-      tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-      tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-  }
-</script>
-// End Javascript for About Me section -> Tab changes
-
-
-// Javascript for burger menu
 const hamburger = document.querySelector(
   ".header .nav-bar .nav-list .hamburger"
 );
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
 const header = document.querySelector(".header.container");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
-// Javascript for burger menu
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
