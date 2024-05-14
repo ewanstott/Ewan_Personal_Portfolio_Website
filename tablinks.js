d264575d87e65b0f4c2480bc95cb3052fdd960a6
Ewan Stott <ewstott@expediagroup.com>
2024-05-14 19:01:56 +0100

    css updates

--- tablinks.js
+++ tablinks.js
@@ -11,3 +11,20 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+// JavaScript for tab switching functionality
+
+// function opentab(tabName) {
+//   var i, tabContent, tabLinks;
+//   tabContent = document.getElementsByClassName("tab-contents");
+//   for (i = 0; i < tabContent.length; i++) {
+//     tabContent[i].style.display = "none";
+//   }
+//   tabLinks = document.getElementsByClassName("tab-links");
+//   for (i = 0; i < tabLinks.length; i++) {
+//     tabLinks[i].className = tabLinks[i].className.replace(" active-link", "");
+//   }
+//   document.getElementById(tabName).style.display = "block";
+//   event.currentTarget.classList.add("active-link"); // Add class to the clicked tab link
+// }
